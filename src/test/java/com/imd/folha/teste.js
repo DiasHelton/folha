ff458eee2e84616d2fd1ebbb8fd9b5c0dc152702
Helton Dias <helton.dias@gmail.com>
2022-01-08 08:35:18 -0300

    html

---
+++ src/test/java/com/imd/folha/teste.js
@@ -0,0 +1,12 @@
+//document.write("Olá Javascript !!!")
+
+// alert("Vamos calcular a area de um terreno...");
+// var a = prompt("Digite o lado a: ");
+// var b = prompt("digite o lado b ");
+// var calculo = a * b;
+// document.write("A área do terreno é de "+ calculo + "m²");
+
+document.write( typeof 1 + "<br />");
+document.write( typeof true + "<br />");
+document.write( typeof 0.0 + "<br />");
+document.write( typeof 34e34 + "<br />");
